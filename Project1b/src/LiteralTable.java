a409a54bcdb002be9ca3136988161c3ac44a206e
HaewonsFarm <ihaewon488@gmail.com>
2025-05-06 19:18:22 +0900

    Initial commit

---
+++ Project1b/src/LiteralTable.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+public class LiteralTable {
+
+    ArrayList<String> literalList;
+    ArrayList<Integer> locationList;
+
+    public void putLiteral(String literal) {
+
+    }
+
+    // 필요 메서드 추가 구현
+}
